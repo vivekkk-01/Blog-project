16fd29a98d1ac869691eb378cc5a562943aa467a
vivek <chimnanivivek14@gmail.com>
2023-05-17 11:06:18 +0530

    Initializing the client

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+
+const router = createBrowserRouter([{ path: "/", element: <Home /> }]);
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
